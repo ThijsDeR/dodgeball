c34b2d68e1cc1b53707dc4ae450e6f4fccfe52b0
ThijsDeR <rech0004@hz.nl>
2021-11-25 11:28:40 +0100

    wow cool update

---
+++ src/Game.ts
@@ -0,0 +1,55 @@
+import GameLoop from "./GameLoop";
+import Scene from "./Scene";
+
+/**
+ * Main class of this Game.
+ */
+export default class Game {
+  public static readonly GRAVITY: number = 0.0098;
+
+  public static readonly FULL_CIRCLE: number = Math.PI * 2;
+
+  public static readonly MIN_BALL_RADIUS: number = 25;
+
+  public static readonly BALL_RADIUS_SCATTER: number = 25;
+
+  public static readonly MIN_BALL_X_SPEED: number = -5;
+
+  public static readonly BALL_X_SPEED_SCATTER: number = 10;
+
+  public static readonly MIN_BALL_Y_SPEED: number = 0;
+
+  public static readonly BALL_POSITION_Y_AREA: number = 0.2;
+
+  public static readonly BALL_COLOR: string = 'blue';
+
+  public static readonly PLAYER_RADIUS: number = 50;
+
+  public static readonly PLAYER_COLOR: string = 'red';
+
+  private scene: Scene;
+
+  private gameLoop: GameLoop;
+  
+
+  /**
+   * Construc a new instance of this class
+   *
+   * @param canvas the canvas to render on
+   */
+  public constructor(canvas: HTMLCanvasElement) {
+    this.scene = new Scene(canvas)
+    this.gameLoop = new GameLoop(this.scene)
+  }
+
+  /**
+   * Start the game.
+   */
+   public start(): void {
+    // Start the animation
+    console.log('start animation');
+    // Set the last tick timestamp to current time
+    this.gameLoop.start()
+  }
+  
+}
